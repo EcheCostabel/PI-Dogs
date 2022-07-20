1851d0792571f2060dc5bb661092de7d2e1e1d17
EcheCostabel <exequielcostabel@gmail.com>
2022-07-20 11:58:58 -0300

    PI

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,9 @@
 const { Router } = require('express');
+const dogs = require('./dogs');
+const dog = require('./dog');
+const idRaza = require('./idRaza');
+const temperament = require('./temperament');
+const temperamentIntermediate = require('./temperamentIntermediate');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +12,12 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+// router.use('/', dogRoutes)
 
+router.use('/dogs', dogs);
+router.use('/dog', dog);
+router.use('/idRaza', idRaza);
+router.use('/temperament', temperament);
+router.use('/intermediate', temperamentIntermediate);
 
 module.exports = router;
